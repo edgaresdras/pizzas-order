7f8ea6100638c26b7380e123ad9af9dd85d8b623
esdrasgreco <esdras_greco@yahoo.com>
2019-02-22 21:13:00 -0600

    primera version de orden de pizzas

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,18 +13,38 @@ pizzaForm.addEventListener('submit', function(event){
   // Previene el default del submit para que no se actualize la página
   event.preventDefault();
 
-  //Preguntamos por el valor de los inputs (Dentro de la función que se ejecuta a raiz del evento de submit)
+//Preguntamos por el valor de los inputs (Dentro de la función que se ejecuta a raiz del evento de submit)
   const pizzaFlavor = pizza.value;
   const sizeValue = pizzaSize.value;
 
-  //Estamos inyectando código HTML a el elemento list (Solo funcionia usando backtips ``)
+let pizzaImg;
+if (pizzaFlavor == 'Carnes Frias') {
+  pizzaImg = 'CarnesFrias.jpg';
+} else if (pizzaFlavor == 'Hawaiana') {
+  pizzaImg = 'Hawaiana.jpg'; 
+}  else {
+  pizzaImg ='Pepperoni.jpg';
+}
+
+let pizzaPrice;
+if (sizeValue == 'Chica') {
+  pizzaPrice = 80;
+} else if (sizeValue == 'Mediana') {
+  pizzaPrice = 100;
+} else if (sizeValue == 'Grande') {
+  pizzaPrice = 120;  
+} else {
+  pizzaPrice = 140;
+}
+
+  //Estamos inyectando código HTML a el elemento list (Solo funciona usando backtips ``)
   list.innerHTML += `
     <div class="pizza-orden">
-      <h3>Pidieron una pizza ${pizzaFlavor}</h3>
-      <p>De tamaño ${sizeValue}</p>
-      <p>Otra cosa</p>
-      <img src="pizza.png">
-      <input id="confirm-button" type="button" value="confirmar">
+      <h3>Pidieron una pizza: ${pizzaFlavor}</h3>
+      <p>De tamaño: ${sizeValue}</p>
+      <p></p>
+      <img src="assets/img/${pizzaImg}">
+      <p>Con precio de: ${pizzaPrice}</p>
     </div>
   `
 });
